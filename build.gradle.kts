00715be40111790006f37198dd02ae2994e479b1
bennyhuo <bennyhuo@kotliner.cn>
2023-07-03 15:44:26 +0800

    Update Kotlin to 1.9.0-RC.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "4.2.0" apply false
-    id("com.android.library") version "4.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.android.application") version "4.2.2" apply false
+    id("com.android.library") version "4.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0-RC" apply false
     id("com.vanniktech.maven.publish") version "0.18.0" apply false
     id("com.github.gmazzo.buildconfig") version "2.1.0" apply false
 }
 
 allprojects {
     repositories {
-        maven { setUrl("https://mirrors.tencent.com/nexus/repository/maven-public/") }
+        mavenCentral()
         maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
     }
 
